76f65c28acb5410403d76a3dffbe0956fa5cf460
bera5186 <rbasu611@gmail.com>
2019-10-21 17:37:48 +0530

    Created Frontend UI, Handled Post Request, added navbar as partials

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const connectDb = require('./config/db')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json({ extended : false }))
 
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-console.log(path.join(__dirname, 'views'))
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
+
+console.log(path.join(__dirname, 'views/partials'))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/', (req, res) => {
-
+    console.log(req.body.data)
+    console.log(new Date())
 })
 
 app.get('/:code', (req, res) => {
